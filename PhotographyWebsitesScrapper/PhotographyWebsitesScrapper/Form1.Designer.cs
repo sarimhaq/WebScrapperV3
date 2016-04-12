4d03917b71846bf7a31007ce4c5abe8ac74fe55b
sarimhaq <sarimhaq@yahoo.com>
2016-04-12 18:35:28 -0400

    Work On Photography Performed

--- PhotographyWebsitesScrapper/PhotographyWebsitesScrapper/Form1.Designer.cs
+++ PhotographyWebsitesScrapper/PhotographyWebsitesScrapper/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- PhotographyWebsitesScrapper/PhotographyWebsitesScrapper/Form1.cs
+++ PhotographyWebsitesScrapper/PhotographyWebsitesScrapper/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace PhotographyWebsitesScrapper
 {
@@ -15,6 +18,120 @@ namespace PhotographyWebsitesScrapper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Regex reg = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+        List<string> EmailAds = new List<string>();
+        List<string> EmailList = new List<string>();
+        const string FilePath = @"C:\Users\sarim\Documents\Visual Studio 2015\Projects\WebScrapperV2\WebScrapperV2\bin\Debug\filekijijiTestLinks.csv";
+        String HomeUrl;
+        String ContactPageUrl;
+
+        public string GetRequestString(string RequestURL)
+        {
+            try
+            {
+                string HTMLPage;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(RequestURL);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                using (StreamReader reader = new StreamReader(resStream))
+                {
+                    HTMLPage = reader.ReadToEnd();
+                }
+
+                return HTMLPage;
+
+            }
+
+            catch (Exception e)
+            {
+
+                return e.Message;
+            }
+
+
+        }
+
+        public WebBrowser BrowserFunction (string HTMLContent)
+        {
+ 
+            WebBrowser SubBrowser = new WebBrowser();
+            SubBrowser.ScriptErrorsSuppressed = true;
+            SubBrowser.DocumentText = "0";
+            SubBrowser.Document.OpenNew(true);
+            SubBrowser.Document.Write(HTMLContent);
+            SubBrowser.Refresh();
+            return SubBrowser;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           using (StreamReader r = new StreamReader(FilePath))
+            {
+                string line;
+                while((line = r.ReadLine()) != null)
+                {
+                    EmailAds.Add(line);
+                }
+            }
+
+            foreach (string link in EmailAds)
+            {
+                HomeUrl = link;
+                string HomePageContent = GetRequestString(link);
+                Match Gold = reg.Match(HomePageContent);
+
+                if(Gold.Value != "")
+                {
+                    EmailList.Add(Gold.Value);
+
+                }
+                else
+                { 
+                WebBrowser HomePageBrowser = new WebBrowser();
+                HomePageBrowser = BrowserFunction(HomePageContent);
+                var SubLinksLeads = HomePageBrowser.Document.GetElementsByTagName("a");
+                foreach(HtmlElement alink in SubLinksLeads)
+                    {
+                        if (alink.InnerHtml != null && alink.GetAttribute("href") != null) { 
+                        if(alink.InnerHtml.Contains("contact") || alink.GetAttribute("href").Contains("contact"))
+                        {
+                            var a = HomeUrl;
+                            string b = alink.GetAttribute("href");
+                            var c = b.Substring(6, b.Length - 6);
+                            string SubPageUrl = a + c;
+
+                            string AboutPageContent = GetRequestString(SubPageUrl);
+                           
+                            MatchCollection Gold2 = reg.Matches(HomePageContent);
+
+                                if (Gold2.Count != 0)
+                            {
+                                foreach(Match GoldMatch in Gold2) { 
+                                EmailList.Add(GoldMatch.Value);
+                                break;
+                                    }
+                                }
+                            else
+                            {
+                                EmailList.Add(HomeUrl + "no contacts");
+                            }
+
+                        }
+                        }
+                    }
+
+                }
+
+
+
+
+
+            }
+            
         }
     }
 }
